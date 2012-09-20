d1d219d090655c855a727e3ffdf9b92662c0463d
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2012-09-20 23:59:03 +0200

    suppression des < p >

--- e-sem/vues/v_informations.php
+++ e-sem/vues/v_informations.php
@@ -62,45 +62,36 @@ return ok;
 		<fieldset class="boxBody"> <legend>Vos informations personnelles</legend>
 
 				<label for="nom">* Nom : </label> <input type="text" id="nom"
-					name="nom" size="30" maxlength="50" />
+					name="nom" size="30" maxlength="50"  required />
 
 				<label for="prenom">* Prénom : </label> <input type="text"
-					id="prenom" name="prenom" size="30" maxlength="50" />
+					id="prenom" name="prenom" size="30" maxlength="50" required />
 				<label for="mail">* Mail : </label> <input type="email" id="mail"
-					name="mail" size="30" maxlength="50" />
+					name="mail" size="30" maxlength="50" required />
 				<label for="academie">Académie : </label> <select id="academie"
 					name="academie" size="1" class='cjComboBox' >
 					<?php
 					foreach ($lesAcademies as $uneAcademie) {
-	                           if($uneAcademie==$lesAcademies[0]){
-	                     ?>
+	          if($uneAcademie==$lesAcademies[0]){ ?>
 					<option value="<?php echo  $uneAcademie['id'] ?>"
-						selected="selected">
-						<?php echo  $uneAcademie["nom"] ?>
+						selected="selected"> <?php echo  $uneAcademie["nom"] ?>
 					</option>
 					<div>
-					<?php
-	                        }
-	                        else{
-	                       ?>
+					<?php }else{ ?>
 					<option value="<?php echo  $uneAcademie['id'] ?>">
 						<?php echo  $uneAcademie["nom"]?>
 					</option>
-					<?php
-	                                }
-	                    }
-
-	                    ?>
+					<?php } } ?>
 				</select>       
 			
 				<label for="residencepersonnelle">* Ville de la résidence
 					personnelle : </label> <input type="text" id="residencepersonnelle"
-					name="residencepersonnelle" size="30" maxlength="50" />
+					name="residencepersonnelle" size="30" maxlength="50" required />
 				<br />		
 				<label for="residenceadministrative">* Ville de la résidence
 					administrative : </label> <input type="text"
 					id="residenceadministrative" name="residenceadministrative"
-					size="30" maxlength="50" />
+					size="30" maxlength="50" required />
 				<br />
 				Titre : <input type="radio" id="titre"name="titre" value="professeur" checked/>Professeur 
 				<input type="radio" id="titre" name="titre" value="ipr">IA-IPR 
@@ -116,24 +107,11 @@ return ok;
 			value="autre">Autre
 			</fieldset>
 </fieldset>
-  
 </div>
-</form>
-
-
-
-
-
-		<!--<div class="piedForm">-->
-		<p>
+<br />
+<div class="piedForm">		
 			<input id="valider" type="submit" value="valider" size="20" />
-
-		</p>
-	</div>
-
-
-
-
+</div>
 </form>
 
 
